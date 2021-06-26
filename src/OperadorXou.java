544bc9c99b2619aa13d7505688720fc47087b3f9
Lucas Kurata <lucas.kurata@hotmail.com>
2021-06-25 21:12:32 -0300

    Implementa operador XOU

--- src/OperadorXou.java
+++ src/OperadorXou.java
@@ -4,7 +4,10 @@ public class OperadorXou {
 
 	public static void main(String[] args) {
 		/*Em uma ONG há uma horta monitorada e automatizada por um arduino.
-		 * Escreva o código de um programa que receba a umidade relativa do ar e a temperatura atual. Caso a umidade seja maior que 70% ou caso o temperatura seja maior que 26°C, exibir a mensagem "ligando os ventiladores". Caso as duas condições sejam verdadeiras ou falsas, exibir a mensagem: "Entrar em contato com agrônomo".*/
+		 * Escreva o código de um programa que receba a umidade relativa do ar e a temperatura 
+		 * atual. Caso a umidade seja maior que 70% ou caso o temperatura seja maior que 26°C, exibir 
+		 * a mensagem "ligando os ventiladores". Caso as duas condições sejam verdadeiras ou falsas, exibir 
+		 * a mensagem: "Entrar em contato com agrônomo".*/
 		Scanner leitor =  new Scanner(System.in);
 		Double umidade, temperatura;
 		System.out.println("Por favor, insira a umidade relativa do ar:");
@@ -13,6 +16,11 @@ public class OperadorXou {
 		temperatura = leitor.nextDouble();
 		
 		//Aqui é preciso criar um if que verifique as duas condições para que o os ventiladores sejam ligados ou não
+		if (umidade > 70 ^ temperatura > 26) {
+			System.out.println("Ligando os Ventiladores!");
+		}else {
+			System.out.println("Por Favor, entrar em contato com um agrônomo");
+		}
 		
 		leitor.close();
 		
